860787e06250e6bf70380e974a9e1ee487913942
Ajay Krishnan <ajaykrishnan@gogosoon.com>
2020-04-24 15:35:18 +0530

    Updates

---
+++ precache-manifest.dd389027a5f5dea3acfe84df777b2a08.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56293a269387bdc64fa8427b8ac0d1ee",
+    "url": "/mysite/index.html"
+  },
+  {
+    "revision": "64165b8f844631cc51b2",
+    "url": "/mysite/static/css/2.053bdb2e.chunk.css"
+  },
+  {
+    "revision": "15a570307791b1d9b208",
+    "url": "/mysite/static/css/main.f8af3074.chunk.css"
+  },
+  {
+    "revision": "64165b8f844631cc51b2",
+    "url": "/mysite/static/js/2.ed3b51e2.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/mysite/static/js/2.ed3b51e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15a570307791b1d9b208",
+    "url": "/mysite/static/js/main.4b889b9e.chunk.js"
+  },
+  {
+    "revision": "02bfcf91d532a8e87297",
+    "url": "/mysite/static/js/runtime-main.46e84ec4.js"
+  },
+  {
+    "revision": "6d4254916a98cedee8e65dd30e7f4ce1",
+    "url": "/mysite/static/media/Con2.6d425491.png"
+  },
+  {
+    "revision": "4b638caa2354895ff6b9108988899893",
+    "url": "/mysite/static/media/ak.4b638caa.jpeg"
+  },
+  {
+    "revision": "ac18cc7be1241a0eaa4619284904b324",
+    "url": "/mysite/static/media/con1.ac18cc7b.jpeg"
+  }
+]);
